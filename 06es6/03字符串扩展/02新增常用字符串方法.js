a2a6844d34fba2865cb66500fea4b6be67451185
myshine63 <1685738560@qq.com>
2019-07-08 09:46:44 +0800

    修改了一些东西

---
+++ 06es6/03字符串扩展/02新增常用字符串方法.js
@@ -0,0 +1,11 @@
+// 字符串是否包含某个字符串
+let a = "hello word";
+console.log(a.includes("hello"));
+// 字符串是否以特定字符开始结尾
+console.log(a.startsWith("hello"));
+console.log(a.endsWith("hello"));
+// 取出首尾空格
+let b = c = d = "  123  ";
+console.log(b.trim(), 11);
+console.log(c.trimStart(), 11);
+console.log(c.trimEnd(), 11);
---
+++ 06es6/05数值/01数值进制.js
@@ -0,0 +1,8 @@
+/*es6中 2进制 0b/0B 开头,8进制 0O/0o开头,16进制 0x/0X开头  */
+let a = 20;
+let b = 0b10100;
+let c = 0o24;
+let d = 0x14;
+console.log(a === b);//true
+console.log(a === c);
+console.log(a === d);
