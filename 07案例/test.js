288eee7cb375f63d485b576546adf64e5248ee74
myshine63 <1685738560@qq.com>
2019-08-08 14:37:20 +0800

    新增了案例

--- 07案例/test.js
+++ 07案例/test.js
@@ -1,20 +1,3 @@
-var arr = [[1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];
-let set = new Set();
-
-function f(arr) {
-    while (arr.length) {
-        let val = arr.shift();
-        if (val instanceof Array) {
-            f(val)
-        } else {
-            set.add(val);
-        }
-    }
-}
-
-f(arr);
-let res = [...set];
-res.sort(function (a, b) {
-    return a - b;
-});
-console.log(res);
+let arr = [1, true, 0, false, undefined, null,-1,"",];
+let b = arr.filter(Boolean);
+console.log(b);
