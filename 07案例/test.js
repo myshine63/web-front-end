f5a912bc73bd50ae5c3665e7e6a8c28d83d303d8
myshine <1685738560@qq.com>
2019-09-01 00:08:43 +0800

    增加了bfc的处理

--- 07案例/test.js
+++ 07案例/test.js
@@ -1,1 +1,22 @@
-console.log("123".padEnd(10,"00"))
+async function f() {
+    let a = await f1();
+    let b = await f2();
+    console.log(a);
+    console.log(b);
+}
+
+function f1() {
+    return "123"
+}
+
+function f2() {
+    setTimeout(function () {
+        console.log(456);
+    }, 1000)
+}
+
+let a = f();
+console.log(a);
+a.then(ss => {
+    console.log(ss)
+});
