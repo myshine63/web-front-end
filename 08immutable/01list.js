d25cfe940e349b784928bd9381c3fb4077ee7cfb
myshine63 <1685738560@qq.com>
2020-01-15 15:03:25 +0800

    增加immutable.js部分案例

--- 08immutable/01list.js
+++ 08immutable/01list.js
@@ -1,27 +1,20 @@
-const {List, Set} = require('immutable');
-
-const list = List([1, 2, 3]);
+const {List, Map} = require('immutable');
 
 function createList() {
   const emptyList = List();
-  const listArr = List([1, 2, 3, 4]);
-  console.log(listArr);// List [ 1, 2, 3, 4 ]
-  console.log(listArr[0]);//undefined
-
-  const set = Set([1, 2, 3, 4]);
-  const lestSet = List(set);
-  console.log(lestSet);// List [ 1, 2, 3, 4 ]
-  List("tom");
-  List.of(..."tom");// List [ "t", "o", "m" ]
-  List.of(1, 2, 3, 4);// List [ 1, 2, 3, 4 ]
-  console.log(lestSet.size);// List.size()获取长度
-  let a =listArr.set(10,5)
+  const arrList = List([1, 2, 3, 4]);
+  const setList = List(new Set([1, 2, 3, 4]));
+  let stringList = List("tom");
+  console.log(1, emptyList);
+  console.log(2, arrList);
+  console.log(3, setList);
+  console.log(4, stringList);
+  let a = List.of(..."tom");// List [ "t", "o", "m" ]
+  let b = List.of(1, 2, 3, 4);// List [ 1, 2, 3, 4 ]
   console.log(a);
-  console.log(a.get(111));
-
+  console.log(b);
 }
 
-// List的set,delete,insert,clear,push,pop,unshift,shift,update,setSize.都不会改变原List而是返回一个新的list
 function listSet() {
   function sum(collection) {
     return collection.reduce((sum, x) => sum + x, 0)
@@ -61,48 +54,126 @@ function flatmap() {
 function zip() {
   const a = List([1, 2, 3, 4, 5]);
   const b = List([4, 5, 5, 5]);
-  let c = a.zipWith((a, b) => { // 遍历次数为最短的list
-    // console.log(a, b)
+  let c = a.zipWith((listA, listB) => { // 遍历次数为最短的list
+    return listA + listB
   }, b);
   console.log(a.zip(b));// 以最短的打包
   console.log(a.zipAll(b)); // 全部打包，缺值得为undefined
-  console.log(a.zip(c))
+  console.log(c)
 }
 
 function flatten() {
-  let list = List([1, 2, [3, 4]]);
-  list.flatten(true);
-  console.log(list);
-  // console.log(list.flatten(fal))
+  let list = List([1, 2, List([3, 4])]);
+  console.log(list.flatten()); //List [ 1, 2, 3, 4 ]
 }
 
 function merge() {
+  const list = List([1, 2, 3]);
   let a = list.merge([1, [2, 3]]);
   console.log(a);
 }
 
 function setIn() {
-  let list = List([1, 2, [3, 4, [5, 6]]]);
+  let list = List([1, 2, [3, 4, List([5, 6])]]);
   let a = list.setIn([2, 2, 1], 7);
-  console.log(a);
+  console.log(a.toJS());
 }
 
 function to() {
+  const list = List([1, 2, 3]);
   let a = list.toJSON();
   let b = list.toJS();
   let c = list.toArray();
   let d = list.toObject();
-  console.log(typeof a);
-  console.log(typeof [].toString());
-  console.log(b);
-  console.log(c);
-  console.log(d);
+  console.log(Array.isArray(a)); //true
+  console.log(Array.isArray(b)); //true
+  console.log(Array.isArray(c)); //true
+  console.log(Array.isArray(d)); //true
 }
 
 function search() {
+  const list = List([1, 2, 3]);
   let a = list.has(0);
   let b = list.includes(1);
   console.log(a, b);
+}
+
+function reduce() {
+  const list = List([1, 2, 3]);
+  let a = list.reduce((count, value) => {
+    return count + value;
+  }, 0);
+  console.log(a);
+}
+
+function sortBy() {
+  const list = List([
+    {id: 9, name: "blue"},
+    {id: 12, name: "green"},
+    {id: 22, name: "black"},
+    {id: 15, name: "red"}
+  ]);
+  let a = list.sortBy(value => value.id);
+  let b = list.sortBy(value => value.name);
+  console.log(a.toJS());
+  console.log(b.toJS());
+}
+
+function sortByGroup() {
+  const list = List([
+    Map({v: 0}),
+    Map({v: 1}),
+    Map({v: 1}),
+    Map({v: 0}),
+    Map({v: 2})
+  ]);
+  const res = list.groupBy(x => x.get('v'))
+  console.log(res)
+}
+
+function splice() {
+  List(['a', 'b', 'c', 'd']).splice(1, 2, 'q', 'r', 's')
+// List [ "a", "q", "r", "s", "d" ]
+}
+
+function test() {
+  const list = List([1, 2, 3]);
+  console.log(list.shift());
+  console.log(list.rest())
+  console.log(list)
+}
+
+function equal() {
+  const a = List([1, List([1])]);
+  const b = List([1, List([1])]);
+  const c = List([1, [1]]);
+  const d = List([1, [1]]);
+  console.log(a.equals(b)); //true
+  console.log(c.equals(d)); //false
+}
+
+function update() {
+  let list = List([1, 2, 3]);
+  let a = list.update(0, value => {
+    return value + 1;
+  });
+  let b = list.update(3, 10, value => {
+    return value + 1;
+  });
+  let c = list.update(3, 10, value => {
+    return value;
+  });
+  let d = list.update(3, value => {
+    return value + 1;
+  });
+  let e = list.update(3, value => {
+    return value;
+  });
+  console.log(a);// List [ 2, 2, 3 ]
+  console.log(b);// List [ 1, 2, 3, 11 ]
+  console.log(c);// List [ 1, 2, 3 ]
+  console.log(d);// List [ 1, 2, 3 , NaN ]
+  console.log(e);// List [ 1, 2, 3 ]
 }
 
 // createList()
@@ -112,6 +183,13 @@ function search() {
 // zip();
 // flatten()
 // merge();
-// setIn()
-to();
+// setIn();
+// to();
 // search();
+// reduce();
+// sortBy();
+// sortByGroup();
+// splice();
+// test();
+// equal();
+// update();
--- 08immutable/02map.js
+++ 08immutable/02map.js
@@ -1,7 +1,7 @@
 const {Map} = require('immutable');
 const base = Map({name: "tom", age: 14});
 
-function map() {
+function createMap() {
   const a = Map();
   const b = Map([[1, 2], [3, 4]]);
   const c = Map({name: "tom"});
@@ -9,7 +9,12 @@ function map() {
   console.log(b);
   console.log(c);
   console.log(c.size);
-  console.log(c.name)
+}
+
+function deleteAll() {
+  const base = Map({name: "tom", age: 14});
+  let a = base.deleteAll(["age"]);
+  console.log(a.toJS()) //{ name: 'tom' }
 }
 
 function merge() {
@@ -18,19 +23,88 @@ function merge() {
   console.log(a);
   console.log(b);
 }
-function mapEntries() {
-  base.mapEntries(value => {
-    console.log(value);
-    return value
-  })
+
+function mergeWith() {
+  const one = Map({a: 10, b: 20, c: 30});
+  const two = Map({b: 40, a: 50, d: 60});
+  one.mergeWith((oldVal, newVal) => oldVal / newVal, two);
+// { "a": 0.2, "b": 0.5, "c": 30, "d": 60 }
+  two.mergeWith((oldVal, newVal) => oldVal / newVal, one);
+// { "b": 2, "a": 5, "d": 60, "c": 30 }
 }
 
-// map();
+function mapEntries() {
+  const base = Map({name: "tom", age: 14});
+  let a = base.mapEntries(([key, value]) => {
+    return [key, value + 1]
+  });
+  console.log(a); // Map { "name": "tom1", "age": 15 }
+}
+
+function setIn() {
+  const one = Map({a: {a: 10}});
+  console.log(one.setIn(['a', 'a'], "1").toJS())
+}
+
+function mergeIn() {
+  const one = Map({a: {name: "tom"}});
+  const two = Map({id: 15});
+  let a = one.mergeIn(['a'], two);
+  console.log(a.toJS()) //{ a: { name: 'tom', id: 15 } }
+}
+
+function mergeDeep() {
+  const one = Map({a: Map({x: 10, y: 10}), b: Map({x: 20, y: 50})})
+  const two = Map({a: Map({x: 2}), b: Map({y: 5}), c: Map({z: 3})})
+  one.mergeDeep(two)
+  // Map {
+  //   "a": Map { "x": 2, "y": 10 },
+  //   "b": Map { "x": 20, "y": 5 },
+  //   "c": Map { "z": 3 }
+  // }
+}
+
+function withMutation() {
+  const base = Map({name: "tom"});
+  let res = base.withMutations(map => {
+    // 这样的操作不会生成多个map对象
+    map.set('name', 'jerry');
+    console.log(map);
+    map.set('id', 15);
+    console.log(map);
+  });
+}
+
+function mapKeys() {
+  const base = Map({name: "tom", age: 14});
+  let a = base.mapKeys((key, value) => {
+    return value + 1;
+  });
+  console.log(a);//Map { "tom1": "tom", 15: 14 }
+}
+function to() {
+  const base = Map({name: "tom", age: 14});
+  let a = base.toJS();
+  let b = base.toJSON();
+  let c = base.toArray();
+  let d = base.toObject();
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+//  { name: 'tom', age: 14 }
+// { name: 'tom', age: 14 }
+// [ [ 'name', 'tom' ], [ 'age', 14 ] ]
+// { name: 'tom', age: 14 }
+}
+
+// createMap();
+// deleteAll();
 // merge();
+// mergeWith();
 // mapEntries();
-const map1 = Map()
-const map2 = map1.withMutations(map => {
-  map.set('a', 1).set('b', 2).set('c', 3)
-});
-assert.equal(map1.size, 0);
-assert.equal(map2.size, 3);
+// setIn();
+// mergeIn();
+// withMutation();
+// mapKeys();
+// to();
